69efae9c84f791f2a80d686146e2ef8bcaa142d6
Shaun Anderton <shaunanderton1@gmail.com>
2024-08-04 15:27:05 +0000

    added functions for project

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// We will need some functions to get our code to run
+
+
+// run game function
+
+// chaeck answer function
+
+// calculate correct answer
+
+// increment score
+
+// increment wrong answer
+
+// display questions

eb62add2e78fe0221f2a7dcfcc4a1a77a1636ac2
Shaun Anderton <shaunanderton1@gmail.com>
2024-08-08 12:47:32 +0000

    added function structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,58 @@
-// We will need some functions to get our code to run
+// WE want some event listeners too 
 
+// Dom content loaded listener
+document.addEventListener("DOMContentLoaded", function() {
+    
+})
+
+// button event listeners
+
+// We will need some functions to get our code to run
 
 // run game function
 
+function runGame() {
+
+}
+
 // chaeck answer function
+
+function checkAnswer() {
+
+}
 
 // calculate correct answer
 
+function calculateCorrectAnswer() {
+
+}
+
 // increment score
+
+function incrementScore() {
+
+}
 
 // increment wrong answer
 
+function incrementWrongAnswer() {
+
+}
+
 // display questions
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractionQuestion() {
+    
+}
+
+function displayMultiplyQuestion() {
+    
+}
+
+function displayDivideQuestion() {
+    
+}

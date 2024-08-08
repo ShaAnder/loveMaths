8aee482d9b3612282e0c057e552bc0593399aad2
Shaun Anderton <shaunanderton1@gmail.com>
2024-08-08 14:31:48 +0000

    added commenting, added score incrementation, added different game mode selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,17 +36,42 @@ function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
+    // check gametype, setup the game screen as shown
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2)
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+    } else if (gameType === "subtract") {
+        displaySubtractionQuestion(num1, num2);
+    } else if (gameType === "division") {
+    displayDivisionQuestion(num1, num2);
+
     } else {
         alert(`Unknown game type: ${gameType}`)
         throw `Unknown game type: ${gameType}. Aborting!`
     }   
 }
 
-// chaeck answer function
-
+/**
+ * checks the user answer based off of the correct calculation
+ */
 function checkAnswer() {
+
+    // get our user score, calculated answer and then our is correct bool
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        // throw alert to tell user correct
+        alert("Correct!");
+    } else {
+        // throw an alert to tell user wrong answer
+        alert(`You answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+    }
+
+    // run the game
+    runGame(calculatedAnswer[1]);
 
 }
 
@@ -55,31 +80,50 @@ function checkAnswer() {
  */
 function calculateCorrectAnswer() {
 
+    // get our operands inner text
     let operand1 = parseInt(document.getElementById('operand1').innerText);
     let operand2 = parseInt(document.getElementById('operand2').innerText);
     let operator = document.getElementById("operator").innerText;
 
+    // run a check to see what the operand is
     if (operator === "+") {
+        // return our correct answer and type as an array]
         return [operand1 + operand2, "addition"];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        return [operand1 - operand2, "divide"];
     } else {
+        // throw an alert / error to stop game
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
 
 }
 
-// increment score
-
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 function incrementScore() {
 
+    // get the element from the dom, then increment it with the old score + 1
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
+
 }
 
-// increment wrong answer
-
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
 
+    // get the element from the dom, then increment it with the old score + 1
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
+    
 }
-
 /**
  * Displays an addition question
  * @param {*} operand1 -> first number randomly generated on run game
@@ -100,6 +144,6 @@ function displayMultiplyQuestion() {
     
 }
 
-function displayDivideQuestion() {
+function displayDivisionQuestion() {
     
 }
